4dd0fabdac3b57f16c0741cd01c8df01395e3eab
Antonio E. Porreca <antonio.porreca@lis-lab.fr>
2023-03-23 23:01:08 +0100

    Quotes

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@
 
 
 // A generator of functional digraphs up to isomorphism. Based on
-// Antonio E. Porreca, Ekaterina Timofeeva, Polynomial-delay
-// generation of functional digraphs up to isomorphism,
+// Antonio E. Porreca, Ekaterina Timofeeva, "Polynomial-delay
+// generation of functional digraphs up to isomorphism",
 // arXiv:2302.13832, 2023, https://doi.org/10.48550/arXiv.2302.13832
 // and on the original funkdinen, a proof-of-concept Python
 // implementation of the same algorithms, available at
